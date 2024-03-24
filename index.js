00b2aa0644ffc9577c210e067c39b48c3b108157
Erin Yant <ghosrider383@gmail.com>
2024-03-24 11:04:26 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+console.log("hello world")
+const main = document.getElementById("main")
+console.log(main)
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory';
+newHeader.textContent = 'Erin is the champion';
+document.body.appendChild(newHeader);
+
+
